0ebeda185ca80b6977426a41fb1954b0c1504364
Lucas De Souza Khenayfis <lucask@ons.org.br>
2021-07-14 10:27:08 -0300

    Move todos os dados internos para mesmo script

--- data-raw/HIDR.R
+++ data-raw/HIDR.R
@@ -26,8 +26,29 @@ dummy[, 4] <- 1 / 1400
 
 HIDR <- rbind(HIDR, dummy)
 
-usethis::use_data(HIDR, internal = TRUE, overwrite = TRUE)
+########################## VAZOES MAXIMAS PARA AJUSTE 
 
+path <- "X:/Ciclo 1 - 2005 a 2014/_Resultados Finais por Usina"
+dirs <- list.dirs(path, full.names = TRUE, recursive = FALSE)
+dirs <- paste0(dirs, "/Niveis de Jusante")
+arqs <- file.path(dirs, "Resultados/img.RData")
+
+temresultado <- file.exists(arqs)
+arqs <- arqs[temresultado]
+arqs <- sub("img.RData", "CoefBase.csv", arqs)
+
+VAZMAX <- lapply(arqs, function(arq) {
+
+    VAZMAX <- read.csv(arq)
+    VAZMAX <- tail(VAZMAX[, 2], 1)
+    cod    <-  read.table(sub("Niveis .*", "CodUsi.txt", arq))[1, 1]
+    data.frame(USI = cod, VAZMAX = VAZMAX)
+})
+
+VAZMAX <- do.call(rbind, VAZMAX)
+VAZMAX <- VAZMAX[order(VAZMAX[, 1]), ]
+
+usethis::use_data(HIDR, VAZMAX, internal = TRUE, overwrite = TRUE)
 
 ########################## EXEMPLO DE DADO IMPORTADO
 
