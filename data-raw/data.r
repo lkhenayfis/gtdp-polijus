773f0f1ca7efc62a4c88540a9ed915b488d44d56
Lucas De Souza Khenayfis <lucask@ons.org.br>
2021-07-21 16:18:28 -0300

    Corrige escalonamento do dado dummy

--- data-raw/data.r
+++ data-raw/data.r
@@ -23,7 +23,7 @@ HIDR <- cbind(USI = HIDR[, 1], VAZEF = vazef, HIDR[, colpvn])
 dummy <- HIDR[1, ]
 dummy$USI <- 999
 dummy$VAZEF <- 950
-dummy[, 3] <- 9
+dummy[, 3] <- 34
 dummy[, 4] <- 1 / 1400
 
 HIDR <- rbind(HIDR, dummy)
